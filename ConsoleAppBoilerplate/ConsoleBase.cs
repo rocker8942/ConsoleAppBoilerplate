6a828bb2e7d7fcdcc90dcfda4ed4d6da07a833dd
joe park <rocker8942@gmail.com>
2022-02-15 21:58:25 +0800

    Add project files.

---
+++ ConsoleAppBoilerplate/ConsoleBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppBoilerplate
+{
+    internal class ConsoleBase
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleAppBoilerplate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog;
+
+namespace ConsoleAppBoilerplate
+{
+    internal class Program
+    {
+        public static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var consoleBase = new ConsoleBase();
+                consoleBase.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Logger.Error(e);
+                throw;
+            }
+        }
+    }
+}
